8f9590632f40b96f1f3a95c43b5d29aedad6f131
Saurabh Gawade <saurabhgawade2920@gmail.com>
2025-06-18 21:54:20 +0530

    loops added

---
+++ src/PracticeSetLoops/PrintNumbers.java
@@ -0,0 +1,8 @@
+package PracticeSetLoops;
+public class PrintNumbers {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/PracticeSetLoops/ReverseNumber.java
@@ -0,0 +1,19 @@
+package PracticeSetLoops;
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        int reverse = 0;
+
+        while (number != 0) {
+            reverse = reverse * 10 + number % 10;
+            number /= 10;
+        }
+
+        System.out.println("Reversed number is " + reverse);
+        scanner.close();
+    }
+}
